a43b9b24245895d7bb573f66c84c9171f243709d
niresh <nireshine@gmail.com>
2025-02-20 01:12:34 +0530

    minor fixes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
                 whileInView={{ opacity: 1, y: 0 }}
                 initial={{ opacity: 0, y: -100 }}
                 transition={{ duration: 0.5 }}
-                className="text-center text-4xl">Get in Touch</motion.h2>
+                className="my-5 text-center text-4xl">Get in Touch</motion.h2>
             <div className="text-center tracking-tighter">
                 <motion.p
                     whileInView={{ opacity: 1, x: 0 }}
